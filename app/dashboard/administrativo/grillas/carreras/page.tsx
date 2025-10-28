3f047cc2b952a50c92371c628b8fcac5167250a6
marcosgoi <goizuemarcos02@gmail.com>
2025-10-28 08:46:22 -0300

    Borro usos de sidebar que dan error.

--- app/dashboard/administrativo/grillas/carreras/page.tsx
+++ app/dashboard/administrativo/grillas/carreras/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Link from 'next/link'
-import Sidebar from '@/components/dashboard/sidebar'
 import { redirect } from 'next/navigation'
 import { Button } from '@/components/ui/button'
 import { createClient } from '@/utils/supabase/server'
@@ -30,7 +29,6 @@ export default async function GrillaCarreras(){
   return (
     <div className="min-h-screen bg-slate-50 dark:bg-slate-900">
       <div className="flex">
-        <Sidebar />
         <main className="flex-1 p-8">
 
           <div className="max-w-6xl mx-auto mt-6">
